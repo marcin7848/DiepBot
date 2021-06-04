2847ac4037598dd2727151576b77a5592b4da6d7
unknown <marcin7848@gmail.com>
2021-06-04 20:33:24 +0200

    points to eat and enemies in threads

--- main.py
+++ main.py
@@ -1,23 +1,52 @@
+import time
+
 from Point import Point
 from Points import Points
+import concurrent.futures
+import queue
+from threading import Thread
+
+queue_enemy = queue.Queue()
+queue_points_to_eat = queue.Queue()
+
+
+def find_enemy(screen, width, height):
+    (enemy_x, enemy_y) = points.detect_closest_enemy(screen, width, height)
+    queue_enemy.put((enemy_x, enemy_y))
+
+
+def find_points_to_eat(screen, width, height):
+    (xPoints, yPoints, points_idx) = points.detect_points_to_eat(screen, width, height)
+    queue_points_to_eat.put((xPoints, yPoints, points_idx))
+
 
 while True:
     points = Points()
     (screen, width, height) = points.get_screen()
 
-    #(xPoints, yPoints, points_idx) = points.detect_points_to_eat(screen, width, height)
-    #for idx in points_idx:
-        #points.add_point(Point([0, 0, 250], xPoints[idx], yPoints[idx]))
+    thread_enemy = Thread(target=find_enemy, args=(screen, width, height))
+    thread_enemy.start()
 
-    #(enemy_x, enemy_y) = points.detect_closest_enemy(screen, width, height)
-    #if enemy_x != -1:
-        #points.add_point(Point([0, 0, 250], enemy_x, enemy_y))
+    thread_points_to_eat = Thread(target=find_points_to_eat, args=(screen, width, height))
+    thread_points_to_eat.start()
+
+    thread_enemy.join()
+    (enemy_x, enemy_y) = queue_enemy.get()
+    if enemy_x != -1:
+        points.add_point(Point([0, 0, 250], enemy_x, enemy_y))
+    else:
+        thread_points_to_eat.join()
+        (xPoints, yPoints, points_idx) = queue_points_to_eat.get()
+        for idx in points_idx:
+            points.add_point(Point([0, 0, 250], xPoints[idx], yPoints[idx]))
 
 
-    (friend_x, friend_y) = points.detect_closest_friend(screen, width, height)
 
-    if friend_x != -1:
-        points.add_point(Point([0, 0, 250], friend_x, friend_y))
+
+    #(friend_x, friend_y) = points.detect_closest_friend(screen, width, height)
+
+    #if friend_x != -1:
+        #points.add_point(Point([0, 0, 250], friend_x, friend_y))
 
     points.draw_points()
 
