66d5bda4e5e90174b61e92418e1a56488bd56539
unknown <marcin7848@gmail.com>
2021-06-04 22:25:02 +0200

    moving done

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import time
 
 from Point import Point
@@ -5,7 +6,6 @@ from Points import Points
 import queue
 from threading import Thread
 import pyautogui
-import random
 
 queue_enemy = queue.Queue()
 queue_point_to_eat = queue.Queue()
@@ -29,30 +29,67 @@ def shooting(screen, width, height):
     thread_points_to_eat.start()
 
     thread_enemy.join()
-    (enemy_x, enemy_y) = queue_enemy.get()
-    if enemy_x != -1:
-        points.add_point(Point([0, 0, 250], enemy_x, enemy_y))
-        pyautogui.moveTo(enemy_x, enemy_y)
+    (enemy_pos_x, enemy_pos_y) = queue_enemy.get()
+    if enemy_pos_x != -1:
+        #points.add_point(Point([0, 0, 250], enemy_x, enemy_y))
+        pyautogui.moveTo(enemy_pos_x, enemy_pos_y)
     else:
         thread_points_to_eat.join()
-        (point_x, point_y) = queue_point_to_eat.get()
-        if point_x != -1:
-            points.add_point(Point([0, 0, 250], point_x, point_y))
-            pyautogui.moveTo(point_x, point_y)
+        (point_eat_x, point_eat_y) = queue_point_to_eat.get()
+        if point_eat_x != -1:
+            #points.add_point(Point([0, 0, 250], point_x, point_y))
+            pyautogui.moveTo(point_eat_x, point_eat_y)
+
+
+def key_press(key, press_down):
+    if press_down:
+        pyautogui.keyDown(key)
+    else:
+        pyautogui.keyUp(key)
+
+
+def moving(screen, width, height):
+    (friend_x, friend_y) = points.detect_closest_friend(screen, width, height)
+
+    if friend_x != -1:
+        center_of_screen_x = int(width / 2)
+        center_of_screen_y = int(height / 2)
+        dist = math.hypot(center_of_screen_x - friend_x, center_of_screen_y - friend_y)
+        if dist > 100:
+            if center_of_screen_x < friend_x:
+                key_press('right', True)
+            else:
+                key_press('right', False)
+            if center_of_screen_x > friend_x:
+                key_press('left', True)
+            else:
+                key_press('left', False)
+            if center_of_screen_y < friend_y:
+                key_press('down', True)
+            else:
+                key_press('down', False)
+            if center_of_screen_y > friend_y:
+                key_press('up', True)
+            else:
+                key_press('up', False)
+        else:
+            key_press('left', False)
+            key_press('right', False)
+            key_press('up', False)
+            key_press('down', False)
+
+        #points.add_point(Point([0, 0, 250], friend_x, friend_y))
 
 
 while True:
     points = Points()
-    (screen, width, height) = points.get_screen()
+    (screenx, widthx, heightx) = points.get_screen()
 
-    thread_shooting = Thread(target=shooting, args=(screen, width, height))
+    thread_shooting = Thread(target=shooting, args=(screenx, widthx, heightx))
     thread_shooting.start()
 
+    thread_moving = Thread(target=moving, args=(screenx, widthx, heightx))
+    thread_moving.start()
 
-    #(friend_x, friend_y) = points.detect_closest_friend(screen, width, height)
-
-    #if friend_x != -1:
-        #points.add_point(Point([0, 0, 250], friend_x, friend_y))
-
-    points.draw_points()
+    #points.draw_points()
 
